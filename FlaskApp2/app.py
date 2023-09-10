f8e9b79b20e1246bc05593c046dd7e7613f881d7
hasnainali659 <hasnainali.ha659@gmail.com>
2023-09-10 12:31:18 +0500

    Flask App Basic

---
+++ FlaskApp2/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/report')
+def report():
+    lower_letter = False
+    upper_letter = False
+    num_end = False
+    
+    user_name = request.args.get('username')
+    
+    lower_letter = any(c.islower() for c in user_name)
+    upper_letter = any(c.isupper() for c in user_name)
+    num_end = user_name[-1].isdigit()
+    
+    report = lower_letter and upper_letter and num_end
+    
+    return render_template('report.html', report = report, lower_letter=lower_letter, upper_letter=upper_letter, num_end=num_end)
+
+if __name__ == '__main__':
+    app.run(debug=True)
