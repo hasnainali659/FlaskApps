bffb117929f768337db5be5497171f0bf39bf65d
hasnainali659 <hasnainali.ha659@gmail.com>
2023-09-29 14:35:57 +0500

    Flask Database

---
+++ FlaskApp6/setup_database.py
@@ -0,0 +1,16 @@
+from basic import db, Puppy, app
+
+app.app_context().push()
+db.create_all()
+
+sam = Puppy('Sammy', 3)
+frank = Puppy('Frankie', 4)
+
+print(sam.id)
+print(frank.id)
+
+db.session.add_all([sam, frank])
+
+db.session.commit()
+print(sam.id)
+print(frank.id)
