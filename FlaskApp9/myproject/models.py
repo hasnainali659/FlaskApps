fc61b439dbc7861b162501bccb6ef3bcb83a40a5
hasnainali659 <hasnainali.ha659@gmail.com>
2023-09-30 10:30:30 +0500

    Flask Blueprint

---
+++ FlaskApp9/myproject/models.py
@@ -0,0 +1,34 @@
+from myproject import db
+
+class Puppy(db.Model):
+
+    __tablename__ = 'puppies'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.Text)
+    owner = db.relationship('Owner',backref='puppy',uselist=False)
+
+    def __init__(self,name):
+        # Note how a puppy only needs to be initalized with a name!
+        self.name = name
+
+
+    def __repr__(self):
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        else:
+            return f"Puppy name is {self.name} and has no owner assigned yet."
+        
+
+class Owner(db.Model):
+
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer,primary_key= True)
+    name = db.Column(db.Text)
+    # We use puppies.id because __tablename__='puppies'
+    puppy_id = db.Column(db.Integer,db.ForeignKey('puppies.id'))
+
+    def __init__(self,name,puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
